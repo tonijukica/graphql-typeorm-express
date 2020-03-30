733d16b1ba263c081caa04010485e0717436395e
U-DESKTOP-58PJ9OE\tjcol <tjcold@hotmail.com>
2020-03-30 21:42:09 +0200

    Add Registration Resolver

---
+++ server/index.ts
@@ -0,0 +1,24 @@
+import * as Express from 'express';
+import { ApolloServer } from 'apollo-server-express';
+import { createConnection } from 'typeorm';
+import { buildSchema } from 'type-graphql';
+import { RegisterResolver } from './Modules/User/Register';
+
+const main = async () => {
+  await createConnection();
+
+  const schema = await buildSchema({
+    resolvers: [RegisterResolver]
+  });
+  
+  const apolloServer = new ApolloServer({schema});
+
+  const app = Express();
+
+   apolloServer.applyMiddleware({ app });
+
+  app.listen(4000, () => {
+    console.log('Server running on http://localhost:4000/graphql');
+  });
+}
+main();
